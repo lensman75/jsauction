0164304a4481f5ff33dcf7e5278eaa59beee6bee
John Doe <anministry.mgmt@gmail.com>
2018-12-11 01:35:11 +0200

    Start working on my project. Created modal window login. Created register form without click functions.

---
+++ script/loginModal.js
@@ -0,0 +1,23 @@
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+function moddisp() {
+  modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+//LoginError
+function loginError() {
+  var para = document.createElement("footer");
+  var node = document.createTextNode("Login error, please do something");
+  var modalContent = document.getElementById('ab');
+  para.appendChild(node);
+  modalContent.appendChild(para);
+}
---
+++ script/registrationModal.js
@@ -0,0 +1,8 @@
+var modal = document.getElementById('myModal');
+var gotoregister = document.getElementById('gotoregister');
+var modalreg = document.getElementById('abc');
+
+function closeLogin() {
+  modal.style.display = "none";
+  modalreg.style.display = "block";
+}
